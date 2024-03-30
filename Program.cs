18b3bd9188860a8d58504b3ed0bbd897d691e15a
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2024-03-30 10:44:39 -0500

    Remove unnecessary steps

--- Program.cs
+++ Program.cs
@@ -8,19 +8,12 @@ namespace Sprite_Fantasia_Extractor
     {
         static void Main(string[] args)
         {
-            Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "\\extracted");
             BinaryReader br = new BinaryReader(File.OpenRead(args[0]));
-            br.BaseStream.Position = 18;
 
-            MemoryStream ms = new();
+            Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "\\extracted");
+            br.BaseStream.Position = 18;
             using (var ds = new DeflateStream(new MemoryStream(br.ReadBytes((int)(br.BaseStream.Length - 18))), CompressionMode.Decompress))
-                ds.CopyTo(ms);
-            br = new(ms);
-            br.BaseStream.Position = 0;
-            using FileStream FS = File.Create(Path.GetDirectoryName(args[0]) + "\\extracted\\" + Path.GetFileName(args[0]));
-            BinaryWriter bw = new(FS);
-            bw.Write(br.ReadBytes((int)ms.Length));
-            bw.Close();
+                ds.CopyTo(File.Create(Path.GetDirectoryName(args[0]) + "\\extracted\\" + Path.GetFileName(args[0])));
         }
     }
 }
